c65deac73833653e467874baeb15871b2ce92ee9
Eva Camino <caminoeva1@gmail.com>
2023-01-09 21:12:15 -0700

    remove notes

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,4 @@
-// Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
@@ -8,7 +8,7 @@ var userChoice = []
 var results = ""
 
 
-// Write password to the #password input
+
 function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
@@ -22,7 +22,7 @@ function generatePassword() {
     var characters = [];
 
     var isUppercase = confirm("Do you want uppercase?")
-    console.log(isUppercase) // true
+    console.log(isUppercase) 
     if (isUppercase === true) {
         characters = characters.concat(uppercase);
     }
@@ -47,27 +47,24 @@ function generatePassword() {
     console.log(passwordLength);
     console.log(characters);
 
-    // generate password using characters array
+
 
 
     for (var i = 0; i < passwordLength; i++){
-        // create a random number to use as the character index
+   
         var randomNumber = Math.floor(Math.random() * characters.length);
-        // console.log(randomNumber);
-        // use the random number to extract a val from the char array
+   
         var randomCharacter = characters[randomNumber];
-        // console.log(randomCharacter);
+       
 
 
-        // insert that value into the results string
+        
         results = results + randomCharacter;
     }
 
-    // var characters = Math.floor(Math.random() characters.length);
-    // console.log(random, months[random]);
-    return results;
+  
 
 }
 
-// Add event listener to generate button
+
 generateBtn.addEventListener("click", writePassword);
